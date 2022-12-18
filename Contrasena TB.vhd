5046ddada0e1268b6fd863b03b7085606f1856f6
SergioBerrendo <99045432+SergioBerrendo@users.noreply.github.com>
2022-12-18 20:05:08 +0100

    Contraseña y LEDS
    se han añadido leds mientras se introduce la contraseña para ver el botón elegido

--- Contrasena TB.vhd
+++ Contrasena TB.vhd
@@ -9,33 +9,38 @@ end top_tb;
 architecture behavior of top_tb is
 	component top2
     port(
-		boton0: in std_logic;
-    	boton1: in std_logic;
+		boton1: in std_logic;
     	boton2: in std_logic;
     	boton3: in std_logic;
+    	boton4: in std_logic;
     	okey: in std_logic;
     	clk: in std_logic;
-    	contrasena: out std_logic_vector(7 downto 0));
+    	contrasena: out std_logic_vector(7 downto 0);
+        LED: out std_logic_vector(3 downto 0)
+        );
+
      end component;
      
      signal okey: std_logic;
-     signal boton0: std_logic;
      signal boton1: std_logic;
      signal boton2: std_logic;
      signal boton3: std_logic;
+     signal boton4: std_logic;
      signal clk: std_logic;
      signal contrasena: std_logic_vector(7 downto 0);
+     signal LED: std_logic_vector(3 downto 0);
      constant k: time := 10 ns;
      
 begin
 	uut: top2 port map(
     	okey=>okey,
-        boton0=>boton0,
         boton1=>boton1,
         boton2=>boton2,
         boton3=>boton3,
+        boton4=>boton4,
         clk=>clk,
-        contrasena=>contrasena
+        contrasena=>contrasena,
+        LED=>LED
         );
         
      okey<='0', '1' after k, '0' after 2*k, '1' after 5*k;
@@ -50,18 +55,18 @@ begin
      
      stimuli: process
      begin
-     	boton0<='0';
-        boton1<='0';
+     	boton1<='0';
         boton2<='0';
         boton3<='0';
+        boton4<='0';
      	wait for 0.25*k;
-     	boton0<='1';
+     	boton2<='1';
      	wait for k;
      	boton1<='1';
      	wait for k;
-     	boton2<='1';
-     	wait for k;
      	boton3<='1';
+     	wait for k;
+     	boton4<='1';
         wait for 4*k;
      end process;
 end behavior;
--- Contrasena.vhd
+++ Contrasena.vhd
@@ -6,13 +6,14 @@ use ieee.numeric_std.ALL;
 entity top2 is
 
 port(
-	boton0: in std_logic;
-    boton1: in std_logic;
+	boton1: in std_logic;
     boton2: in std_logic;
     boton3: in std_logic;
+    boton4: in std_logic;
     okey: in std_logic;
     clk: in std_logic;
-    contrasena: out std_logic_vector(7 downto 0)
+    contrasena: out std_logic_vector(7 downto 0);
+    LED: out std_logic_vector(3 downto 0)
 
     );
 end top2;
@@ -28,94 +29,163 @@ begin
       	current_state <= next_state;
     end process;
     
-    nextstate_decod: process (boton0, boton1, boton2, boton3, current_state) 
+    nextstate_decod: process (boton1, boton2, boton3, boton4, okey, current_state) 
     variable comodin: std_logic_vector(1 downto 0);
     begin 
     	next_state <= current_state; 
         case current_state is
         when S0 => 
         	contrasena <= (OTHERS => '0');
-        	if rising_edge(boton0) then 
+            LED <= (OTHERS => '0');
+        	if rising_edge(boton1) then 
             	comodin(0) := '0';
                 comodin(1) := '0';
+                LED(0)<='1';
+                LED(1)<='0';
+                LED(2)<='0';
+                LED(3)<='0';
             	next_state <= S1; 
-             elsif rising_edge(boton1) then 
+             elsif rising_edge(boton2) then 
             	comodin(0) := '1';
                 comodin(1) := '0';
-            	next_state <= S1;
-             elsif rising_edge(boton2) then 
-            	comodin(0) := '0';
-                comodin(1) := '1';
+                LED(0)<='1';
+                LED(1)<='1';
+                LED(2)<='0';
+                LED(3)<='0';
             	next_state <= S1;
              elsif rising_edge(boton3) then 
+            	comodin(0) := '0';
+                comodin(1) := '1';
+                LED(0)<='1';
+                LED(1)<='1';
+                LED(2)<='1';
+                LED(3)<='0';
+            	next_state <= S1;
+             elsif rising_edge(boton4) then 
             	comodin(0) := '1';
                 comodin(1) := '1';
+                LED(0)<='1';
+                LED(1)<='1';
+                LED(2)<='1';
+                LED(3)<='1';
             	next_state <= S1;
             end if;
   		when S1 => 
         	contrasena(0) <= comodin(0);
             contrasena(1) <= comodin(1);
-        	if rising_edge(boton0) then 
+        	if rising_edge(boton1) then 
             	comodin(0) := '0';
                 comodin(1) := '0';
+                LED(0)<='1';
+                LED(1)<='0';
+                LED(2)<='0';
+                LED(3)<='0';
             	next_state <= S2; 
-             elsif rising_edge(boton1) then 
+             elsif rising_edge(boton2) then 
             	comodin(0) := '1';
                 comodin(1) := '0';
-            	next_state <= S2;
-             elsif rising_edge(boton2) then 
-            	comodin(0) := '0';
-                comodin(1) := '1';
+                LED(0)<='1';
+                LED(1)<='1';
+                LED(2)<='0';
+                LED(3)<='0';
             	next_state <= S2;
              elsif rising_edge(boton3) then 
+            	comodin(0) := '0';
+                comodin(1) := '1';
+                LED(0)<='1';
+                LED(1)<='1';
+                LED(2)<='1';
+                LED(3)<='0';
+            	next_state <= S2;
+             elsif rising_edge(boton4) then 
             	comodin(0) := '1';
                 comodin(1) := '1';
+                LED(0)<='1';
+                LED(1)<='1';
+                LED(2)<='1';
+                LED(3)<='1';
             	next_state <= S2;
             end if;
         when S2 => 
         	contrasena(2) <= comodin(0);
             contrasena(3) <= comodin(1);
-        	if rising_edge(boton0) then 
+        	if rising_edge(boton1) then 
             	comodin(0) := '0';
                 comodin(1) := '0';
+                LED(0)<='1';
+                LED(1)<='0';
+                LED(2)<='0';
+                LED(3)<='0';
             	next_state <= S3; 
-             elsif rising_edge(boton1) then 
+             elsif rising_edge(boton2) then 
             	comodin(0) := '1';
                 comodin(1) := '0';
-            	next_state <= S3;
-             elsif rising_edge(boton2) then 
-            	comodin(0) := '0';
-                comodin(1) := '1';
+                LED(0)<='1';
+                LED(1)<='1';
+                LED(2)<='0';
+                LED(3)<='0';
             	next_state <= S3;
              elsif rising_edge(boton3) then 
+            	comodin(0) := '0';
+                comodin(1) := '1';
+                LED(0)<='1';
+                LED(1)<='1';
+                LED(2)<='1';
+                LED(3)<='0';
+            	next_state <= S3;
+             elsif rising_edge(boton4) then 
             	comodin(0) := '1';
                 comodin(1) := '1';
+                LED(0)<='1';
+                LED(1)<='1';
+                LED(2)<='1';
+                LED(3)<='1';
             	next_state <= S3;
             end if;
         when S3 => 
         	contrasena(4) <= comodin(0);
             contrasena(5) <= comodin(1);
-        	if rising_edge(boton0) then 
+        	if rising_edge(boton1) then 
             	comodin(0) := '0';
                 comodin(1) := '0';
+                LED(0)<='1';
+                LED(1)<='0';
+                LED(2)<='0';
+                LED(3)<='0';
             	next_state <= S4; 
-             elsif rising_edge(boton1) then 
+             elsif rising_edge(boton2) then 
             	comodin(0) := '1';
                 comodin(1) := '0';
-            	next_state <= S4;
-             elsif rising_edge(boton2) then 
-            	comodin(0) := '0';
-                comodin(1) := '1';
+                LED(0)<='1';
+                LED(1)<='1';
+                LED(2)<='0';
+                LED(3)<='0';
             	next_state <= S4;
              elsif rising_edge(boton3) then 
+            	comodin(0) := '0';
+                comodin(1) := '1';
+                LED(0)<='1';
+                LED(1)<='1';
+                LED(2)<='1';
+                LED(3)<='0';
+            	next_state <= S4;
+             elsif rising_edge(boton4) then 
             	comodin(0) := '1';
                 comodin(1) := '1';
+                LED(0)<='1';
+                LED(1)<='1';
+                LED(2)<='1';
+                LED(3)<='1';
             	next_state <= S4;
             end if;
         when S4 =>
  			contrasena(6) <= comodin(0);
             contrasena(7) <= comodin(1);
             if rising_edge(okey) then
+            	LED(0)<='0';
+                LED(1)<='0';
+                LED(2)<='0';
+                LED(3)<='0';
             	next_state <= S5;
              end if;
         when S5 =>
@@ -124,7 +194,7 @@ begin
         when others => 
         	next_state <= current_state;
     		contrasena <= (OTHERS => '0');
-
+			LED <= (OTHERS => '0');
         end case;
     end process;
  
