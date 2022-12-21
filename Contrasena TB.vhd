e2c4dcc863054fb0ea866e9e034a7127f6e1699c
SergioBerrendo <99045432+SergioBerrendo@users.noreply.github.com>
2022-12-21 11:00:49 +0100

    Contraseña con LEDs y enable
    Contraseña con LEDs para ver el número asignado a cada botón, y enable para habilitar la escritura

--- Contrasena TB.vhd
+++ Contrasena TB.vhd
@@ -3,11 +3,11 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use ieee.numeric_std.ALL;
 
-entity top_tb is
-end top_tb;
+entity FormadorPalabra_tb is
+end FormadorPalabra_tb;
 
-architecture behavior of top_tb is
-	component top2
+architecture behavior of FormadorPalabra_tb is
+	component FormadorPalabra
     port(
 		boton1: in std_logic;
     	boton2: in std_logic;
@@ -15,8 +15,9 @@ architecture behavior of top_tb is
     	boton4: in std_logic;
     	okey: in std_logic;
     	clk: in std_logic;
+        enable_fp: in std_logic;
     	contrasena: out std_logic_vector(7 downto 0);
-        LED: out std_logic_vector(3 downto 0)
+        led_contrasena: out std_logic_vector(3 downto 0)
         );
 
      end component;
@@ -27,12 +28,13 @@ architecture behavior of top_tb is
      signal boton3: std_logic;
      signal boton4: std_logic;
      signal clk: std_logic;
+     signal enable_fp: std_logic;
      signal contrasena: std_logic_vector(7 downto 0);
-     signal LED: std_logic_vector(3 downto 0);
+     signal led_contrasena: std_logic_vector(3 downto 0);
      constant k: time := 10 ns;
      
 begin
-	uut: top2 port map(
+	uut: FormadorPalabra port map(
     	okey=>okey,
         boton1=>boton1,
         boton2=>boton2,
@@ -40,7 +42,8 @@ begin
         boton4=>boton4,
         clk=>clk,
         contrasena=>contrasena,
-        LED=>LED
+        led_contrasena=>led_contrasena,
+        enable_fp=>enable_fp
         );
         
      okey<='0', '1' after k, '0' after 2*k, '1' after 5*k;
@@ -55,17 +58,31 @@ begin
      
      stimuli: process
      begin
+     	--enable apagado
      	boton1<='0';
         boton2<='0';
         boton3<='0';
         boton4<='0';
+        wait for 0.2*k;
+        boton1<='1';
+        wait for 0.2*k;
+        boton1<='0';
+        boton2<='1';
+        wait for 0.2*k;
+        boton2<='0';
+        boton3<='1';
+        wait for 0.2*k;
+        boton3<='0';
+        --se enciende el enable
+        enable_fp<='1';
      	wait for 0.25*k;
+     	boton3<='1';
+     	wait for 0.5*k;
      	boton2<='1';
      	wait for k;
+        enable_fp<='1';
      	boton1<='1';
-     	wait for k;
-     	boton3<='1';
-     	wait for k;
+     	wait for 0.5*k;
      	boton4<='1';
         wait for 4*k;
      end process;
--- Contrasena.vhd
+++ Contrasena.vhd
@@ -3,22 +3,21 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use ieee.numeric_std.ALL;
 
-entity top2 is
-
+entity FormadorPalabra is
 port(
-	boton1: in std_logic;
+	boton1: in std_logic;--botones con los que introducir la contraseña
     boton2: in std_logic;
     boton3: in std_logic;
     boton4: in std_logic;
-    okey: in std_logic;
-    clk: in std_logic;
+    okey: in std_logic;--boton para confirmar contraseña
+    clk: in std_logic;--señal de reloj
+    enable_fp: in std_logic;--habilita la escritura de la contraseña
     contrasena: out std_logic_vector(7 downto 0);
-    LED: out std_logic_vector(3 downto 0)
-
+    led_contrasena: out std_logic_vector(3 downto 0)--leds que se encienden para indicar el numero introducido
     );
-end top2;
+end FormadorPalabra;
 
-architecture behavioral of top2 is 
+architecture behavioral of FormadorPalabra is 
 	type STATES is (S0, S1, S2, S3, S4, S5); 
     signal current_state: STATES := S0; 
     signal next_state: STATES; 
@@ -26,176 +25,128 @@ architecture behavioral of top2 is
 begin
 	state_register: process (clk) 
 	begin
-      	current_state <= next_state;
+      	current_state <= next_state;--lleva a cabo los cambios de estado
     end process;
     
-    nextstate_decod: process (boton1, boton2, boton3, boton4, okey, current_state) 
+    nextstate_decod: process (boton1, boton2, boton3, boton4, okey, enable_fp, current_state) 
     variable comodin: std_logic_vector(1 downto 0);
     begin 
     	next_state <= current_state; 
+    if enable_fp then--si esta habilitado se puede escribir la contraseña
+        next_state <= current_state; --lleva a cabo los cambios de estado
         case current_state is
-        when S0 => 
-        	contrasena <= (OTHERS => '0');
-            LED <= (OTHERS => '0');
+        when S0 => --ningun numero introducido
+        	contrasena <= (OTHERS => '0');--contraseña a cero
+            led_contrasena <= (OTHERS => '0');--leds apagados
         	if rising_edge(boton1) then 
-            	comodin(0) := '0';
+            	comodin(0) := '0';--sirve para actualizar correctamente la contraseña en funcion del boton elegido
                 comodin(1) := '0';
-                LED(0)<='1';
-                LED(1)<='0';
-                LED(2)<='0';
-                LED(3)<='0';
-            	next_state <= S1; 
+                led_contrasena<="0001";--leds correspondientes al boton
+            	next_state <= S1; --estado sucesor
              elsif rising_edge(boton2) then 
             	comodin(0) := '1';
                 comodin(1) := '0';
-                LED(0)<='1';
-                LED(1)<='1';
-                LED(2)<='0';
-                LED(3)<='0';
+                led_contrasena<="0011";
             	next_state <= S1;
              elsif rising_edge(boton3) then 
             	comodin(0) := '0';
                 comodin(1) := '1';
-                LED(0)<='1';
-                LED(1)<='1';
-                LED(2)<='1';
-                LED(3)<='0';
+                led_contrasena<="0111";
             	next_state <= S1;
              elsif rising_edge(boton4) then 
             	comodin(0) := '1';
                 comodin(1) := '1';
-                LED(0)<='1';
-                LED(1)<='1';
-                LED(2)<='1';
-                LED(3)<='1';
+                led_contrasena<="1111";
             	next_state <= S1;
             end if;
-  		when S1 => 
+  		when S1 => --1 digito introducido
         	contrasena(0) <= comodin(0);
             contrasena(1) <= comodin(1);
         	if rising_edge(boton1) then 
             	comodin(0) := '0';
                 comodin(1) := '0';
-                LED(0)<='1';
-                LED(1)<='0';
-                LED(2)<='0';
-                LED(3)<='0';
+                led_contrasena<="0001";
             	next_state <= S2; 
              elsif rising_edge(boton2) then 
             	comodin(0) := '1';
                 comodin(1) := '0';
-                LED(0)<='1';
-                LED(1)<='1';
-                LED(2)<='0';
-                LED(3)<='0';
+                led_contrasena<="0011";
             	next_state <= S2;
              elsif rising_edge(boton3) then 
             	comodin(0) := '0';
                 comodin(1) := '1';
-                LED(0)<='1';
-                LED(1)<='1';
-                LED(2)<='1';
-                LED(3)<='0';
+                led_contrasena<="0111";
             	next_state <= S2;
              elsif rising_edge(boton4) then 
             	comodin(0) := '1';
                 comodin(1) := '1';
-                LED(0)<='1';
-                LED(1)<='1';
-                LED(2)<='1';
-                LED(3)<='1';
+                led_contrasena<="1111";
             	next_state <= S2;
             end if;
-        when S2 => 
+        when S2 => --2 digitos introducidos
         	contrasena(2) <= comodin(0);
             contrasena(3) <= comodin(1);
         	if rising_edge(boton1) then 
             	comodin(0) := '0';
                 comodin(1) := '0';
-                LED(0)<='1';
-                LED(1)<='0';
-                LED(2)<='0';
-                LED(3)<='0';
+                led_contrasena<="0001";
             	next_state <= S3; 
              elsif rising_edge(boton2) then 
             	comodin(0) := '1';
                 comodin(1) := '0';
-                LED(0)<='1';
-                LED(1)<='1';
-                LED(2)<='0';
-                LED(3)<='0';
+                led_contrasena<="0011";
             	next_state <= S3;
              elsif rising_edge(boton3) then 
             	comodin(0) := '0';
                 comodin(1) := '1';
-                LED(0)<='1';
-                LED(1)<='1';
-                LED(2)<='1';
-                LED(3)<='0';
+                led_contrasena<="0111";
             	next_state <= S3;
              elsif rising_edge(boton4) then 
             	comodin(0) := '1';
                 comodin(1) := '1';
-                LED(0)<='1';
-                LED(1)<='1';
-                LED(2)<='1';
-                LED(3)<='1';
+                led_contrasena<="1111";
             	next_state <= S3;
             end if;
-        when S3 => 
+        when S3 => --3 digitos introducidos
         	contrasena(4) <= comodin(0);
             contrasena(5) <= comodin(1);
         	if rising_edge(boton1) then 
             	comodin(0) := '0';
                 comodin(1) := '0';
-                LED(0)<='1';
-                LED(1)<='0';
-                LED(2)<='0';
-                LED(3)<='0';
+                led_contrasena<="0001";
             	next_state <= S4; 
              elsif rising_edge(boton2) then 
             	comodin(0) := '1';
                 comodin(1) := '0';
-                LED(0)<='1';
-                LED(1)<='1';
-                LED(2)<='0';
-                LED(3)<='0';
+                led_contrasena<="0011";
             	next_state <= S4;
              elsif rising_edge(boton3) then 
             	comodin(0) := '0';
                 comodin(1) := '1';
-                LED(0)<='1';
-                LED(1)<='1';
-                LED(2)<='1';
-                LED(3)<='0';
+                led_contrasena<="0111";
             	next_state <= S4;
              elsif rising_edge(boton4) then 
             	comodin(0) := '1';
                 comodin(1) := '1';
-                LED(0)<='1';
-                LED(1)<='1';
-                LED(2)<='1';
-                LED(3)<='1';
+                led_contrasena<="1111";
             	next_state <= S4;
             end if;
-        when S4 =>
+        when S4 => --4 digitos introducidos
  			contrasena(6) <= comodin(0);
             contrasena(7) <= comodin(1);
             if rising_edge(okey) then
-            	LED(0)<='0';
-                LED(1)<='0';
-                LED(2)<='0';
-                LED(3)<='0';
+            	led_contrasena<="0000";
             	next_state <= S5;
              end if;
-        when S5 =>
+        when S5 => --no se si sobra
         	--fin de escritura de contrasena
             contrasena <= contrasena;
         when others => 
         	next_state <= current_state;
     		contrasena <= (OTHERS => '0');
-			LED <= (OTHERS => '0');
+			led_contrasena <= (OTHERS => '0');
         end case;
+    end if;
     end process;
  
 end behavioral;
