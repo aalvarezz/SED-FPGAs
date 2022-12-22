25ccda0addec2e895f06297f4387278bde7afe43
LuisFcoG <luis.gtorres@alumnos.upm.es>
2022-12-22 10:21:23 +0100

    Nuevos Componentes
    Detector de flanco y sincronizador

---
+++ synchrnzr.vhd
@@ -0,0 +1,24 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity SYNCHRNZR is
+    port (
+        CLK : in std_logic;
+        ASYNC_IN : in std_logic;
+        SYNC_OUT : out std_logic
+    );
+end SYNCHRNZR;
+
+architecture BEHAVIORAL of SYNCHRNZR is
+    
+    signal sreg : std_logic_vector(1 downto 0);
+
+begin
+    process (CLK)
+    begin
+        if rising_edge(CLK) then
+            sync_out <= sreg(1);
+            sreg <= sreg(0) & async_in;
+        end if;
+    end process;
+end BEHAVIORAL;
